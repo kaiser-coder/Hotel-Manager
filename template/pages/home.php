a5644cb49eecef354d9c77398328ed22064237d1
kaiser-coder <miarintsoajohary@gmail.com>
2021-04-04 16:00:17 +0200

    rectification des bugs du menu de navigation

--- template/pages/home.php
+++ template/pages/home.php
@@ -1,3 +1,11 @@
+<h4 class="font-weight-bold py-3 mb-0">Dashboard</h4>
+<div class="text-muted small mt-0 mb-4 d-block breadcrumb">
+	<ol class="breadcrumb">
+		<li class="breadcrumb-item"><a href="#"><i class="feather icon-home"></i></a></li>
+		<li class="breadcrumb-item"><a href="#">Library</a></li>
+		<li class="breadcrumb-item active">Data</li>
+	</ol>
+</div>
 <div class="row">
 	<!-- 1st row Start -->
 	<div class="col-lg-5">
